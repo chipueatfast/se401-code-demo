e7441e73b86673634b5f5e99103ed7391e699c3d
Shyn1803 <anhtranvan610@gmail.com>
2021-12-12 09:44:15 +0700

    Factory method pattern

---
+++ src-factory-method/concreteProducts/ConcreteProduct2.ts
@@ -0,0 +1,11 @@
+
+import { IProduct } from "../IProduct/IProduct";
+
+/**
+* Concrete Products provide various implementations of the Product interface.
+*/
+export class ConcreteProduct2 implements IProduct {
+  public operation(): string {
+    return '{Result of the ConcreteProduct2}';
+  }
+}
